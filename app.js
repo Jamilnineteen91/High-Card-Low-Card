0ec49ca8f5622fe5cc7aaf363396147ce17286ec
Jamil Mbabaali <jamilnineteen91@gmail.com>
2019-04-08 19:59:41 -0500

    started js code

---
+++ app.js
@@ -0,0 +1,7 @@
+let scores, card;
+
+function init () {
+    document.querySelector("player-title").textContent="0";
+    document.getElementById("player-title-0").textContent="Player 1";
+    document.getElementById("player-title-1").textContent="AI";
+}
